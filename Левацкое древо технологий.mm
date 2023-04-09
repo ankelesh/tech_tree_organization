56c353a9a7ffc5db1eabde9899e615ff0abd69ba
ankelesh <ankeleshbnet@inbox.ru>
2023-04-09 16:06:06 +0300

    Management and ideological basics

--- Левацкое древо технологий.mm
+++ Левацкое древо технологий.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.9.13">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Личный уровень" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1679859360898" VGAP_QUANTITY="3 pt">
+<node TEXT="Личный уровень" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1681041445865" VGAP_QUANTITY="3 pt">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <hook NAME="MapStyle" background="#3c3836">
     <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" fit_to_viewport="false"/>
@@ -10,7 +10,7 @@
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-18.75 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-18 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
 <richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
@@ -95,7 +95,7 @@
     </p>
   </body>
 </html></richcontent>
-<node TEXT="Организация 1 уровень" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" POSITION="right" ID="ID_1570526671" CREATED="1679857396322" MODIFIED="1679859428171" VGAP_QUANTITY="3 pt" HGAP_QUANTITY="152.75 pt" VSHIFT_QUANTITY="69.75 pt"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<node TEXT="Организация 1 уровень" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" POSITION="right" ID="ID_1570526671" CREATED="1679857396322" MODIFIED="1681041449048" VGAP_QUANTITY="3 pt"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
 <html>
   <head>
     
@@ -106,7 +106,7 @@
     </p>
   </body>
 </html></richcontent>
-<node TEXT="Малая касса" ID="ID_80983957" CREATED="1679858240036" MODIFIED="1680853793894" HGAP_QUANTITY="-246.24999 pt" VSHIFT_QUANTITY="141 pt"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<node TEXT="Малая касса" ID="ID_80983957" CREATED="1679858240036" MODIFIED="1681041421601"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
 <html>
   <head>
     
@@ -127,11 +127,76 @@
   </body>
 </html></richcontent>
 </node>
+<node TEXT="Партийное\организационное расписание" ID="ID_1676922237" CREATED="1681038280333" MODIFIED="1681041449046"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1) День собрания. Он может быть нефиксированным, к примеру, только по особым новостям.
+    </p>
+    <p>
+      2) Ведение архивов встреч. Нужны для быстрого нахождения мнения команды по тому или иному поводу. Обязательна система тегов или быстрого поиска.
+    </p>
+    <p>
+      3) Ведение системы благонадёжности. Отслеживание полезной и бесполезной деятельности.
+    </p>
+    <p>
+      4) Организация базового сбора информации. Данные о членах организации. Кто где живёт, кто может дать информацию из первых рук.
+    </p>
+    <p>
+      5) Сбор аналитики. Уточнение часовой нормы (в организации обязана быть норма рабочих часов которые её члены уделяют организации. По собственному опыту это не более 2 часов в неделю на человека, но эту норму можно и нужно уточнять и индивидуализировать).
+    </p>
+    <p>
+      6) Приём и выписка членов организации. Приём - начать составлять досье, ознакомить человека с материалами организации (желательно учебными), принять решение, нужен ли он в организации (или его лучше оставить просто подписчиком). Выписка - собрать обратную связь, убрать человека из всех чатов, подписок на облака, разобраться с членскими взносами, донести для всех что человек выбыл.
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node TEXT="Финансовая грамотность" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_840100270" CREATED="1679857562564" MODIFIED="1679858331013" HGAP_QUANTITY="-180.99999 pt" VSHIFT_QUANTITY="105 pt">
-<arrowlink COLOR="#ff6666" TRANSPARENCY="255" DESTINATION="ID_1570526671" STARTINCLINATION="-167.25 pt;-3 pt;" ENDINCLINATION="-63 pt;11.25 pt;"/>
-<arrowlink COLOR="#ff6666" TRANSPARENCY="255" DESTINATION="ID_80983957" STARTINCLINATION="-128.25 pt;0 pt;" ENDINCLINATION="-12.75 pt;0.75 pt;"/>
-<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<node TEXT="Аналитический клуб" ID="ID_51861887" CREATED="1681043487876" MODIFIED="1681045327401"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1) Подготовка краткого обзора недавних новостей.
+    </p>
+    <p>
+      2) Краткий обзор идеологически близких мнений.
+    </p>
+    <p>
+      3) Символический анализ обстановки. Разделение на важные для организации новости и неважные.
+    </p>
+    <p>
+      4) Пассивный анализ обстановки в странах действия.
+    </p>
+    <p>
+      5) Раз в некоторое время проводить идеологическую синхронизацию. Поговорить и обсудить прошлые срачи, свести их к единой позиции.
+    </p>
+    <p>
+      6) Формулировка вопросов к теории на основе попыток её применения к мировой ситуации основываясь на публичных источниках.
+    </p>
+    <p>
+      7) Выработка целей ближайшего спектра - задачи на ближайший месяц.
+    </p>
+    <p>
+      8) Выработка стратегической цели.
+    </p>
+    <p>
+      9) Выработка стратегии и целей по самоулучшению.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Финансовая грамотность" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_840100270" CREATED="1679857562564" MODIFIED="1681041417503">
+<arrowlink COLOR="#ff6666" TRANSPARENCY="255" DESTINATION="ID_80983957" STARTINCLINATION="-127.5 pt;0 pt;" ENDINCLINATION="-12 pt;0.75 pt;"/>
+<arrowlink COLOR="#ff6666" TRANSPARENCY="255" DESTINATION="ID_1570526671" STARTINCLINATION="-166.5 pt;-2.25 pt;" ENDINCLINATION="-62.25 pt;11.25 pt;"/>
+<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
 <html>
   <head>
     
@@ -148,6 +213,57 @@
     </p>
   </body>
 </html></richcontent>
+</node>
+<node TEXT="Базовые навыки менеджмента" POSITION="right" ID="ID_1847044064" CREATED="1681037792057" MODIFIED="1681041450112" HGAP_QUANTITY="-414.24999 pt" VSHIFT_QUANTITY="-130.5 pt">
+<arrowlink COLOR="#ff6666" DESTINATION="ID_1676922237" STARTINCLINATION="75.75 pt;-3.75 pt;" ENDINCLINATION="-48.75 pt;-2.25 pt;"/>
+<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1) Умение вести досье
+    </p>
+    <p>
+      2) Тайм-менеджмент. Умение организовать расписание.
+    </p>
+    <p>
+      3) Минимальные навыки социологии. Умение составлять опросы, умение организовывать общение на электронных платформах.
+    </p>
+    <p>
+      4) Умение работать с конфликтами.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Идеологическая грамотность" POSITION="left" ID="ID_1812719" CREATED="1681041372439" MODIFIED="1681045544826">
+<arrowlink COLOR="#ff6666" DESTINATION="ID_51861887" STARTINCLINATION="-64.5 pt;0 pt;" ENDINCLINATION="-91.5 pt;18.75 pt;"/>
+<richcontent CONTENT-TYPE="xml/" TYPE="DETAILS" HIDDEN="true">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1) Знакомство с идеологическими основами (Манифест компартии, хотя бы основы Маркса, Ленина, Сталина).
+    </p>
+    <p>
+      2) Базовый набор логики.
+    </p>
+    <p>
+      3) Основы экономики. Знание мейнстримных и немейнстримных школ экономики хотя бы на уровне общих знаний.
+    </p>
+    <p>
+      4) Знание локальной политики. Знание конституции. Знать законодательство (позволительны мета-знания). Знать локальных представителей власти в местах нахождения. Знать членов исполнительной власти в местах нахождения членов организации.
+    </p>
+    <p>
+      5) Знание идеологически близких источников и знание контридеологических источников.
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
 </node>
 </map>
